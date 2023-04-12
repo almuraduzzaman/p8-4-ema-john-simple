937899955b5ee825c03501e0fba19ca6cc2a23e8
almuraduzzaman <almuraduzzaman.eee@gmail.com>
2023-04-12 15:13:39 +0600

    first commit

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Product from '../Product/Product';
+import './Cart.css';
+
+const Cart = ({ cart }) => {
+    // const { cart } = props;
+    console.log(cart);
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+
+    const tax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShipping + tax;
+    return (
+        <div className='cart'>
+            <h4>order summary</h4>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <p>Grand Total: ${grandTotal.toFixed(2)}</p>
+        </div>
+    );
+};
+
+export default Cart;
